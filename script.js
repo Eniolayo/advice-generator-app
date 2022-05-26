77a8c352e393b47a1417b53eca9505b8f35180a5
Eniolayo <eniolayodeji@gmail.com>
2022-05-26 21:47:56 +0100

    increase to the advice numbers and a little animation on the dice

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
 const adviceNumber = document.querySelector(".advice-number");
+
 const advice = document.querySelector(".advice");
+
 const dice = document.querySelector(".dice");
-dice.addEventListener("click", (e) => {
-  rollDice();
-});
-function rollDice() {
-  const randomNumber = Math.ceil(Math.random() * 200);
+
+const diceImg = document.querySelector(".dice img");
+
+dice.addEventListener("click", () => {
+  diceImg.style.animation = "rollDice 1s linear alternate";
+  const randomNumber = Math.ceil(Math.random() * 210);
   fetch(`https://api.adviceslip.com/advice/${randomNumber}`)
     .then((data) => data.json())
     .then((target) => {
       adviceNumber.textContent = target.slip.id;
       advice.textContent = target.slip.advice;
     });
-}
+});
+
+rollDice();
